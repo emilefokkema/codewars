b63d948b60ffa5a11333fa30f5d717701066dce9
Emile Fokkema <emilefokkema@gmail.com>
2018-01-19 10:27:40 +0100

    another one

---
+++ snail.js
@@ -0,0 +1,7 @@
+(function(){
+	//-------- https://www.codewars.com/kata/snail -----------
+	var snail = function(array) {
+	  // enjoy
+	};
+	Test.assertDeepEquals(snail([[1,2],[4,3]]), [1,2,3,4]);
+})();
--- test.js
+++ test.js
@@ -4,7 +4,37 @@
 			throw 'expected ' + v1 + ' to equal '+v2;
 		}
 	};
+	var assertDeepEquals = function(o1, o2, msg, onFail){
+		onFail = onFail || function(msg){
+			throw msg;
+		};
+		if(typeof o1 !== "object" && typeof o2 !== "object"){
+			if(o1 !== o2){
+				msg = msg || "expected "+o1+" to equal "+o2;
+				onFail(msg);
+			}
+		}
+		if(typeof o1 !== "object"){
+			onFail("expected "+o1+" to deep equal "+JSON.stringify(o2));
+		}
+		if(typeof o2 !== "object"){
+			onFail("expected "+o2+" to deep equal "+JSON.stringify(o1));
+		}
+		var p;
+		msg = msg || "expected "+JSON.stringify(o1)+" to deep equal "+JSON.stringify(o2);
+		for(p in o1){
+			if(o1.hasOwnProperty(p)){
+				assertDeepEquals(o1[p], o2[p], msg, onFail);
+			}
+		}
+		for(p in o2){
+			if(o2.hasOwnProperty(p)){
+				assertDeepEquals(o2[p], o1[p], msg, onFail);
+			}
+		}
+	};
 	window.Test = {
-		assertEquals: assertEquals
+		assertEquals: assertEquals,
+		assertDeepEquals: assertDeepEquals
 	};
 })();

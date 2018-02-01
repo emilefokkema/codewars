559e65bbd881254855dee05321af9475c1a21ea9
Emile Fokkema <emilefokkema@gmail.com>
2018-02-01 19:16:24 +0100

    closer

--- point-in-polygon.js
+++ point-in-polygon.js
@@ -1,16 +1,6 @@
 (function(){
 	//------- https://www.codewars.com/kata/point-in-polygon-1/ -------------
-	function Quadrant(index){
-		this.index = index;
-	}
-	Quadrant.prototype.distance = function(other){
-		var d = 0, otherIndex = other.index;
-		while(otherIndex != this.index){
-			otherIndex = (otherIndex+1) % 4;
-			d++;
-		}
-		return d;
-	};
+	
 	function Point(p){
 		this.x = p[0];
 		this.y = p[1];
@@ -18,36 +8,40 @@
 	Point.prototype.minus = function(p){
 		return new Point([this.x - p.x, this.y - p.y]);
 	};
-	Point.prototype.getQuadrant = function(){
-		if(this.x >= 0){
-			if(this.y >= 0){
-				return new Quadrant(0);
+	Point.prototype.cross = function(p){
+		return this.x * p.y - this.y * p.x;
+	};
+	Point.prototype.rotateQuarter = function(){
+		return new Point([-this.y, this.x]);
+	};
+	Point.prototype.quadrantAngleFrom = function(p){
+		var self = this;
+		while(p.x < 0 || p.y < 0){
+			p = p.rotateQuarter();
+			self = self.rotateQuarter();
+		}
+		if(self.x > 0){
+			if(self.y > 0){
+				return 0;
 			}
-			return new Quadrant(3);
+			return -1;
 		}
-		if(this.y >= 0){
-			return new Quadrant(1);
+		if(self.y > 0){
+			return 1;
 		}
-		return new Quadrant(2);
+		var cross = new Point([0,0]).minus(p).cross(self.minus(p));
+		return cross < 0 ? 2 : -2;
 	};
 	function pointInPoly(poly, point) {
 	  poly = poly.map(p => new Point(p)), point = new Point(point);
-	  var newQuadrant, 
-	  	startQuadrant = poly[0].minus(point).getQuadrant(),
-	  	currentQuadrant = startQuadrant,
-	  	totalDistance = 0;
-	  for(var i=1;i<poly.length;i++){
-	  	newQuadrant = poly[i].minus(point).getQuadrant();
-	  	totalDistance += newQuadrant.distance(currentQuadrant);
-	  	currentQuadrant = newQuadrant;
-	  }
-	  totalDistance += startQuadrant.distance(currentQuadrant);
-	  console.log(totalDistance);
+	  
 	}
 	var poly = [
 	  [-5, -5], [5, -5],
 	  [5, 5], [-5, 5]
 	];
-	Test.expect(!pointInPoly(poly, [-6,0]));
-	Test.expect(pointInPoly(poly, [1,1]))
+	Test.assertEquals(new Point([-1.1,-1]).quadrantAngleFrom(new Point([1,1])), 2);
+	Test.assertEquals(new Point([-0.9,-1]).quadrantAngleFrom(new Point([1,1])), -2);
+	//Test.expect(!pointInPoly(poly, [-6,0]));
+	//Test.expect(pointInPoly(poly, [1,1]))
 })()
